f96c0f932a938ee9ff2d793e4f4b8b51faef9851
mindl3ss <justtodesign@gmail.com>
2024-11-28 10:41:45 +0100

    fix: add inbox

--- android/src/main/java/com/react/SmsModule.java
+++ android/src/main/java/com/react/SmsModule.java
@@ -82,11 +82,11 @@ public class SmsModule extends ReactContextBaseJavaModule /*implements LoaderMan
             String sortOrder = filterJ.has("sortOrder") ? filterJ.optString("sortOrder") : null;
             long maxDate = filterJ.has("maxDate") ? filterJ.optLong("maxDate") : -1;
             long minDate = filterJ.has("minDate") ? filterJ.optLong("minDate") : -1;
-            Uri telephoneContentUri = Telephony.Sms.CONTENT_URI + uri_filter;
+            Uri telephoneContentUri = Telephony.Sms.CONTENT_URI;
 
             // Log the selection and sortOrder for debugging
             Log.d("SMS", "filter: " + filter);
-            Cursor cursor = mReactContext.getContentResolver().query(telephoneContentUri, null, selection, null, sortOrder);
+            Cursor cursor = mReactContext.getContentResolver().query(telephoneContentUri + "/inbox", null, selection, null, sortOrder);
             int c = 0;
             Log.d("SMS", "cursor count: " + cursor.getCount());
             JSONArray jsons = new JSONArray();
